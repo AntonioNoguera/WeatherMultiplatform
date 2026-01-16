953161a310f2dfe87d05c7c84bb4299222b75624
Michael Noguera <michael.noguera.dev@gmail.com>
2026-01-15 23:27:47 -0600

    Update MainActivity.kt

--- androidApp/src/main/java/com/weather/app/android/MainActivity.kt
+++ androidApp/src/main/java/com/weather/app/android/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.*
 import com.weather.app.android.presentation.WeatherScreen
-
+//should implement the newest shared vm, the previous are really bad
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
